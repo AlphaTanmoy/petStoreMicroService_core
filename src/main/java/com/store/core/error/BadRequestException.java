8cceffa51ccb6eb084b9cf40d0cbd3f12b6a0734
alphaTanmoy <dtanmoy169@gmail.com>
2025-01-19 16:32:30 +0530

    added files to admin

---
+++ src/main/java/com/store/core/error/BadRequestException.java
@@ -0,0 +1,23 @@
+package com.store.core.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BadRequestException extends RuntimeException {
+    private String errorMessage;
+    private Integer errorCode; // Custom error code
+    private String errorType;  // Custom error type
+
+    public BadRequestException(String message) {
+        this.errorMessage = message;
+    }
+
+}
+
