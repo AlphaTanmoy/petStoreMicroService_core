b5f2d5d0618c8d3845fc154f14007bdb603808e4
alphaTanmoy <dtanmoy169@gmail.com>
2025-02-09 18:18:28 +0000

    added enums

---
+++ src/main/java/com/store/core/enums/MICROSERVICE.java
@@ -0,0 +1,12 @@
+package com.store.core.enums;
+
+public enum MICROSERVICE {
+    ADMIN,
+    AUTHENTICATION,
+    CHAT,
+    DELIVERY,
+    CORE,
+    PAYMENT,
+    SELLER,
+    USER
+}
---
+++ src/main/java/com/store/core/enums/RESPONSE_TYPE.java
@@ -0,0 +1,6 @@
+package com.store.core.enums;
+
+public enum RESPONSE_TYPE {
+    SUCCESS,
+    FAIL
+}
